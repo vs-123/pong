6af4d6f053776b328481351c86188a57e7a13ed3
VS-123 <>
2026-03-24 06:44:12 +0000

    pong: basic window

--- src/main.c
+++ src/main.c
@@ -1,9 +1,22 @@
 #include "raylib.h"
 #include <stdio.h>
 
+/********************/
+/*  COLOUR PALETTE  */
+/********************/
+
+#define COLOUR_BACKGROUND ((struct Color){.r = 33, .b = 33, .g = 33, .a = 255})
+
 int
 main (void)
 {
-	printf ("Hello, World!\n");
+	InitWindow (800, 800, "Pong -- vs-123");
+
+   while (!WindowShouldClose()) {
+      BeginDrawing();
+      ClearBackground(COLOUR_BACKGROUND);
+      EndDrawing();
+   }
+
 	return 0;
 }
