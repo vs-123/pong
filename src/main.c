7dcd661f84d09dc2362108999ed8caf09c9f4ecf
VS-123 <>
2026-03-24 07:06:05 +0000

    pong: struct-oriented approach

--- src/main.c
+++ src/main.c
@@ -5,18 +5,40 @@
 /*  COLOUR PALETTE  */
 /********************/
 
-#define COLOUR_BACKGROUND ((struct Color){.r = 33, .b = 33, .g = 33, .a = 255})
+#define COLOUR_BACKGROUND                                                      \
+	((struct Color){ .r = 33, .b = 33, .g = 33, .a = 255 })
+
+struct game_t
+{
+	Color bg_colour;
+};
+
+struct game_t
+game_init (void)
+{
+	return (struct game_t){
+		.bg_colour = COLOUR_BACKGROUND,
+	};
+}
+
+void
+game_render (struct game_t *game)
+{
+	ClearBackground (COLOUR_BACKGROUND);
+}
 
 int
 main (void)
 {
 	InitWindow (800, 800, "Pong -- vs-123");
+	struct game_t game = game_init ();
 
-   while (!WindowShouldClose()) {
-      BeginDrawing();
-      ClearBackground(COLOUR_BACKGROUND);
-      EndDrawing();
-   }
+	while (!WindowShouldClose ())
+		{
+			BeginDrawing ();
+			game_render (&game);
+			EndDrawing ();
+		}
 
 	return 0;
 }
